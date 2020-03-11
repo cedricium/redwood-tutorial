6c1261922432a2624fb1d75af9d1fa7a91dd9bed
Cedricium <amaya.cedric@gmail.com>
2020-03-10 17:59:06 -0700

    Create and use BlogLayout in existing pages

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,9 +1,10 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
 const AboutPage = () => {
   return (
-    <div>
-      <h1>AboutPage</h1>
-      <p>Find me in ./web/src/pages/AboutPage/AboutPage.js</p>
-    </div>
+    <BlogLayout>
+      <p>This is a super awesome thing! I &lt;3 Redwood.js :D</p>
+    </BlogLayout>
   )
 }
 
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,9 +1,8 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>HomePage</h1>
-      <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
-    </div>
+    <BlogLayout><p>Home</p></BlogLayout>
   )
 }
 
